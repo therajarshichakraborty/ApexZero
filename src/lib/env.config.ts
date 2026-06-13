b9e335bdfbab316c7cc6a85282db9dc0f4f5308d
Rajarshi Chakraborty <rajarshichakraborty2005@gmail.com>
2026-06-13 15:29:12 +0530

    feat : setup corsair

--- src/lib/env.config.ts
+++ src/lib/env.config.ts
@@ -9,6 +9,7 @@ const envSchema = z.object({
   GITHUB_CLIENT_ID: z.string().optional(),
   GITHUB_CLIENT_SECRET: z.string().optional(),
   NEXT_PUBLIC_APP_URL: z.string().url().optional(),
+  CORSAIR_KEK:z.string()
 });
 
 export const env =
